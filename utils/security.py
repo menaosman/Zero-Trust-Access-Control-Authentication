35f36dd902ae89e98ccb285623dd932bf645bea3
Mena Mohamed Osman <104127946+menaosman@users.noreply.github.com>
2025-10-19 02:39:52 +0300

    Update security.py

--- utils/security.py
+++ utils/security.py
@@ -1,1 +1,12 @@
+from argon2 import PasswordHasher
 
+
+ph = PasswordHasher()
+
+
+def hash_password(pw: str) -> str:
+return ph.hash(pw)
+
+
+def verify_password(stored_hash: str, candidate: str) -> bool:
+return ph.verify(stored_hash, candidate)
